e1f0ddfb7e5a81ac22561e6c46b4646760ba439e
Emmanuel Xavier <emmanuelxavier040@gmail.com>
2023-05-12 21:07:54 +0200

    Kafka Producer + new views integrated

---
+++ src/main/scala/kafka/producer/CsvToKafkaProducer.scala
@@ -0,0 +1,47 @@
+package kafka.producer
+
+import java.util.Properties
+import java.io._
+
+import org.apache.kafka.clients.producer._
+
+object CsvToKafkaProducer {
+
+  val topicName = "test"
+  val bootstrapServers = "localhost:9092"
+
+  def main(args: Array[String]): Unit = {
+
+    val props = new Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
+    // Create Kafka producer
+    val producer = new KafkaProducer[String, String](props)
+
+    // Read CSV file and send records to Kafka
+    val csvFilePath = "E:\\DIS\\kafka\\bin\\windows\\last_twelve_months\\202109-divvy-tripdata.csv"
+    val file = new File(csvFilePath)
+    val br = new BufferedReader(new FileReader(file))
+    var line: String = null
+
+    try {
+      line = br.readLine()
+      while (line != null) {
+
+        println(line)
+        val record = new ProducerRecord[String, String](topicName, line)
+
+        producer.send(record)
+        line = br.readLine()
+        Thread.sleep(3000)
+      }
+    } catch {
+      case e: IOException => e.printStackTrace()
+    } finally {
+      producer.close()
+      br.close()
+    }
+  }
+}
